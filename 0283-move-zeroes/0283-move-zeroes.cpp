ab88d3b3c67d2a28d8ef0297dbb274369eb7e9d9
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-11-27 12:19:44 +0530

    Time: 25 ms (90.86%), Space: 19.4 MB (14.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
+        // int j = 0;
+        // for(int i = 0; i<nums.size(); i++)
+        // {
+        //     if(nums[i]!=0)
+        //         nums[j++] = nums[i];            
+        // }
+        // for(;j<nums.size();j++)
+        //     nums[j] = 0;
+        
+        // snowball method
+        int snowball = 0;
         for(int i = 0; i<nums.size(); i++)
         {
-            if(nums[i]!=0)
-                nums[j++] = nums[i];            
+            if(nums[i]==0)
+                snowball++;
+            else if(snowball>0)
+            {
+                int t = nums[i];
+                nums[i] = 0;
+                nums[i-snowball] = t;
+            }
         }
-        for(;j<nums.size();j++)
-            nums[j] = 0;
     }
 };
