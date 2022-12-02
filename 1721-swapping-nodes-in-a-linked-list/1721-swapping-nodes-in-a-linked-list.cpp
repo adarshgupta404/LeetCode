dadff23bd744a1927fef8b8b502931149022d5af
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-12-02 14:47:49 +0530

    Time: 980 ms (72.88%), Space: 180.2 MB (55.80%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,26 +10,15 @@
  */
 class Solution {
 public:
-    /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-    void setNodes(ListNode* &node, ListNode* &prev, int n)
+    ListNode* lastnode(ListNode* head)
     {
-        int pos = 1;
-        while (pos!=n){
-            pos++;
-            prev = node;
-            node = node->next;
+        ListNode* temp = head;
+        while(temp->next->next!=NULL)
+        {
+            temp = temp->next;
         }
+        return temp;
     }
-
     int countNodes(ListNode* head)
     {
         int c = 0;
@@ -41,61 +30,87 @@ public:
         }
         return c;
     }
+    ListNode* kthFromEnd(ListNode* head, int k) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(k--)
+        {
+            fast=fast->next;
+        }
+        if(!fast)
+            return head->next;
+        while(fast->next)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    ListNode* kthFromStart(ListNode* head, int k)
+    {
+        ListNode* fast = head;
+        while(k>2)
+        {
+            fast=fast->next;
+            k--;
+        }
+        return fast;
+    }
     ListNode* swapNodes(ListNode* head, int k) {
         if(head==NULL)
             return head;
         if(head->next == NULL)
             return head;
-        
         int n = countNodes(head);
-        // fix k
+        if(k==1 || k==n)
+        {
+            if(head->next->next==NULL)
+            {
+                ListNode* temp = head;
+                temp = temp->next;
+                temp->next = head;
+                head->next = NULL;
+                head = temp;
+            }
+            else
+            {
+                ListNode* temp = head->next;
+                ListNode* tail = lastnode(head);
+                ListNode* last = tail->next;
+                last->next = temp;
+                tail->next = head;
+                head->next = NULL;
+                head = last;   
+            }
+        }
+        else
+        {
         if (k>n/2)
             k=n-k+1;
         // IF WE HAVE ODD NUMBER OF ELEMENTS, SWAPPING MIDDLE NODE DOESN'E MAKES SENSE
         if (n%2!=0 && k==(n+1)/2)  
             return head;
-
-        if(k==1 || k==n)
+        ListNode* kthnodebehind = kthFromStart(head, k);
+        ListNode* lkthnodebehind = kthFromEnd(head, k);
+        ListNode* kthnode = kthnodebehind->next;
+        ListNode* lkthnode = lkthnodebehind->next;
+        ListNode* kthnodenext = kthnodebehind->next->next;
+        ListNode* lkthnodenext = lkthnodebehind->next->next;
+        
+        if(kthnode == lkthnodebehind)
         {
-            ListNode* tail = head;
-            ListNode* prev = head;
-            while (tail->next!=NULL){
-                prev = tail;
-                tail=tail->next;
-            }
-
-            prev->next = head;  // Second last -> head
-            tail->next = head->next; // last -> 2nd node
-            head->next = NULL; // detach head, as it is the last node now
-            
-            return tail;    // Tail is the new head
+            kthnodebehind->next = lkthnode;
+            kthnode->next = lkthnode->next;
+            lkthnode->next = kthnode;
         }
         else
         {
-
-            // First node at pos k
-            ListNode* firstNode=head , *firstNodePrev = head ;
-            setNodes(firstNode,firstNodePrev,k) ;
-
-            // Second node at position n-k+1
-            ListNode* secondNode=head, *secondNodePrev = head;
-            setNodes(secondNode,secondNodePrev,n-k+1) ;
-
-            // IF ADJACENT NODES
-            if (firstNode->next==secondNode){
-                firstNode->next = secondNode->next;
-                firstNodePrev->next = secondNode;
-                secondNode->next = firstNode;
-            }
-            else{
-                ListNode* temp = secondNode->next;
-                secondNode->next = firstNode->next;
-                firstNodePrev->next = secondNode;
-
-                secondNodePrev->next = firstNode;
-                firstNode->next = temp;
-            }
+            kthnodebehind->next = lkthnode;
+            kthnode->next = lkthnodenext;
+            lkthnode->next = kthnodenext;
+            lkthnodebehind->next = kthnode;
         }
-        return head;
-    }
+        }
+        return head;
+    }
 };
