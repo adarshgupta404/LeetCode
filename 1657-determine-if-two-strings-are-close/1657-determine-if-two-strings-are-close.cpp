1936854fdf59759076eb5149b5abcb90c597e55c
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-12-03 05:39:13 +0530

    Time: 483 ms (5.05%), Space: 42.3 MB (5.34%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -13,8 +13,10 @@ public:
             w2[i - 'a']++;
             s2.insert(i);
         }
-        sort(w1.begin(), w1.end());
-        sort(w2.begin(), w2.end());
-        return (w1==w2 && s1==s2); 
+        // sort(w1.begin(), w1.end());
+        // sort(w2.begin(), w2.end());
+        // return (w1==w2 && s1==s2);
+        return multiset(w1.begin(),w1.end()) == multiset(w2.begin(),w2.end()) &&
+               unordered_set(word1.begin(),word1.end()) == unordered_set(word2.begin(),word2.end());
     }
 };
