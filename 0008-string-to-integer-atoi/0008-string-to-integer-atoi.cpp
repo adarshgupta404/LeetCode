05a79ffc85df3160fe840adc86daf13b8a40a0c1
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-12-05 08:27:12 +0530

    Time: 7 ms (52.16%), Space: 7.1 MB (12.27%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        
-        // helper variables
-        int res=0;
-        int i=0;
-        int sign=1;
-		
+        long long int result = 0;
+        int sign = 1;
+        int i = 0;
         while(i<s.size()&&s[i]==' ')i++;  //ignore leading white space
-        
-        if(s[i]=='-'||s[i]=='+')          //check if number positve or negative
+        if(s[i]=='+'||s[i]=='-')
+            sign = (s[i++]=='+')? 1:-1;
+        while('0'<=s[i] && s[i]<='9')
         {
-            sign=s[i]=='-'?-1:1;
-            i++;
+            int digit = (s[i++]-'0')*sign;
+            if(sign==1 && (result>INT_MAX/10 || (result==INT_MAX/10 && digit>INT_MAX%10))) 
+                return INT_MAX; //check for overflow
+            if(sign==-1 &&(result<INT_MIN/10 || (result==INT_MIN/10 && digit<INT_MIN%10))) 
+                return INT_MIN; //check for underflow
+            result = result*10 + digit;
         }
-        // now iterate across digits if any
-		// should only be in range 0-9
-        while(i<s.length()&&(s[i]>='0'&&s[i]<='9'))  //traverse string till nondigit not found or string ends
-        {
-            int digit=(s[i]-'0')*sign;
-            if(sign==1 && (res>INT_MAX/10 || (res==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX; //check for overflow
-            if(sign==-1 &&(res<INT_MIN/10 || (res==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN; //check for underflow
-            
-            res=res*10+digit; // update res
-            i++;
-        }
-    
-    return res;
+        return result;
     }
 };
