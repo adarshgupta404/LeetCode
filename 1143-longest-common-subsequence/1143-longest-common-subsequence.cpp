e7e7761ae89912fe16baab2925c031d7d0f8dec5
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-12-15 15:45:09 +0530

    Time: 18 ms (84.85%), Space: 8.5 MB (89.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,9 +6,11 @@ public:
         {
             for(int j = 0; j<b.size(); j++)
             {
-                arr[i+1][j+1] = a[i]==b[j]?arr[i][j]+1:max(arr[i+1][j], arr[i][j+1]);
+                // arr[i+1][j+1] = a[i]==b[j]?arr[i][j]+1:max(arr[i+1][j], arr[i][j+1]);
+                arr[!(i % 2)][j + 1] = a[i] == b[j] ? arr[i % 2][j] + 1 : max(arr[i % 2][j + 1], arr[!(i % 2)][j]);
             }
         }
-        return arr[a.size()][b.size()];
+        // return arr[a.size()][b.size()];
+        return arr[a.size() % 2][b.size()];
     }
 };
