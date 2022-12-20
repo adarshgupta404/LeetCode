460ab9da58a5e4af2ba7ba2dcb92201230d24446
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-12-20 18:33:27 +0530

    Time: 5 ms, Memory: 6.3 MB - LeetHub

---
+++ 0add-binary/0add-binary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int  i = a.length()-1, j = b.length()-1, c = 0;
+        while(i>=0||j>=0||c)
+        {
+            if(i>=0)
+                c += a[i--] - '0';
+            if(j>=0)
+                c += b[j--] - '0';
+            ans += c%2 + '0';
+            c = c/2;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
