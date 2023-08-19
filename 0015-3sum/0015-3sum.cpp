836411960d5ec37a04e3773b0d3a01449695be70
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-08-19 17:12:53 +0530

    Time: 1057 ms (28.35%), Space: 191.7 MB (6.25%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> ans;
-    unordered_map<int, int> m;
-    sort(nums.begin(), nums.end());    
-    
-    int n = nums.size();
-    
-    if (n < 3) return {}; 
-    if (nums[0] > 0) return {}; 
-    
-    for (int i = 0; i < n; i++)
-        m[nums[i]] = i;
-    
-    for (int i = 0; i < n - 2; i++) {
-        if (i != 0 && nums[i] == nums[i - 1]) continue;
-        if (nums[i] > 0) return ans;
-        
-        for (int j = i + 1; j < n - 1; j++) {
-            
-            if (j != i + 1 && nums[j] == nums[j - 1]) continue; 
-            // update j to avoid duplicate triplets
-            
-            int key = -(nums[i] + nums[j]);
-            if (m.find(key) != m.end() && m[key] > j) 
-            // check the index of key > j, so we dont count any duplicate element
-                ans.push_back({nums[i], nums[j], key});
+    int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
         }
-    }
-    return ans;
+        for(auto triplets : s)
+            output.push_back(triplets);
+        return output;
     }
 };
