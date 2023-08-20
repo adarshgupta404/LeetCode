be1d0631b7a1cb89727ad4cd3cd0e03862e89716
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-08-21 00:17:46 +0530

    Time: 9 ms (83.05%), Space: 8.3 MB (75.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,14 +3,27 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         // Brute Force Approach
-        unordered_set<char> set;
+        // unordered_set<char> set;
+        // int l = 0, r = 0, res = 0;
+        // while(r<n){
+        //     while(set.find(s[r])!=set.end()){
+        //         set.erase(s[l]);
+        //         l++;
+        //     }
+        //     set.insert(s[r]);
+        //     res = max(res, r-l+1);
+        //     r++;
+        // }
+        // return res;
+
+        //Optimized Solution
+        vector<int> v(256,-1);
         int l = 0, r = 0, res = 0;
         while(r<n){
-            while(set.find(s[r])!=set.end()){
-                set.erase(s[l]);
-                l++;
+            if(v[s[r]]!=-1){
+                l = max(l, v[s[r]]+1);
             }
-            set.insert(s[r]);
+            v[s[r]] = r;
             res = max(res, r-l+1);
             r++;
         }
