b098482aaa5b3e45710ce6421722787fde468ce3
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-08-22 01:44:14 +0530

    Time: 3 ms (98.39%), Space: 7.2 MB (15.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> v(128, 1);
+        vector<int> v(128);
         int maxl = 0;
         if(s.size()==0)
             return 0;
         int l = 0, r = 0;
         for(int r = 0; r<s.size(); r++){
-            maxl = max(maxl, v[s[r]]++);
+            maxl = max(maxl, ++v[s[r]]);
             if(r-l+1-maxl>k)
                 v[s[l++]]--;
         }
