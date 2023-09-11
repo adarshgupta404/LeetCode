c7f92a5164ddebb3aad08498949670f08550d356
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-09-11 07:12:59 +0530

    Time: 3 ms (72.58%), Space: 9.4 MB (71.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int lowRow = 0;
-                int highRow = matrix.size() - 1;
-                        
-                                while (lowRow < highRow) {
-                                            int mid = lowRow + (highRow - lowRow) / 2;
-                                                        if (matrix[mid][0] == target) {
-                                                                        return true;
-                                                                                    }
-                                                                                                if (matrix[mid][0] < target && target < matrix[mid + 1][0]) {
-                                                                                                                lowRow = mid;
-                                                                                                                                break;
-                                                                                                                                            }
-                                                                                                                                                        if (matrix[mid][0] < target) {
-                                                                                                                                                                        lowRow = mid + 1;
-                                                                                                                                                                                    } else {
-                                                                                                                                                                                                    highRow = mid - 1;
-                                                                                                                                                                                                                }
-                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                        int lowCol = 0;
-                                                                                                                                                                                                                                                int highCol = matrix[0].size() - 1;
-                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                while (lowCol <= highCol) {
-                                                                                                                                                                                                                                                                            int mid = lowCol + (highCol - lowCol) / 2;
-                                                                                                                                                                                                                                                                                        if (matrix[lowRow][mid] == target) {
-                                                                                                                                                                                                                                                                                                        return true;
-                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                                if (matrix[lowRow][mid] < target) {
-                                                                                                                                                                                                                                                                                                                                                lowCol = mid + 1;
-                                                                                                                                                                                                                                                                                                                                                            } else {
-                                                                                                                                                                                                                                                                                                                                                                            highCol = mid - 1;
-                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                                                                                                                                                return false;
+        int lrow = 0, hrow = matrix.size()-1;
+        while(lrow<hrow){
+            int mid = lrow + (hrow-lrow)/2;
+            if(matrix[mid][0]==target)
+                return true;
+            if(matrix[mid][0]<target && matrix[mid+1][0]>target){
+                lrow = mid;
+                break;
+            }
+            if(matrix[mid][0]<target)
+                lrow = mid + 1; 
+            else
+                hrow = mid - 1;
+        }
+        int l = 0, h = matrix[0].size()-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if (matrix[lrow][mid] == target) 
+                return true;
+            if(matrix[lrow][mid]<target){
+                l = mid + 1;
+            }
+            else {
+                h = mid - 1;
+            }
+        }
+        return false;
     }
 };
