38f33c419680ae17a986193fcf428a17b64b59c9
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-10-10 07:25:38 +0530

    Time: 272 ms (82.47%), Space: 129 MB (61.33%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -1,22 +1,33 @@
 class TimeMap {
+    unordered_map<string, vector<pair<int, string>>> mp;
 public:
-    map<string,map<int,string>>mp;
-    TimeMap() {
-        mp=map<string,map<int,string>>();
-    }
+    TimeMap() {}
     
     void set(string key, string value, int time) {
-        mp[key].insert({-1*time,value});
+        // mp[key].insert({-1*time,value});
+        mp[key].push_back({time,value});
     }
     
     string get(string key, int time) {
-        if(mp.find(key)==mp.end()) return "";
-
-        auto it=mp[key].lower_bound(-1*time);
-
-        if(it==mp[key].end()) return "";
-
-        return it->second;
+        // if(mp.find(key)==mp.end()) return "";
+        // auto it=mp[key].lower_bound(-1*time);
+        // if(it==mp[key].end()) return "";
+        // return it->second;
+        if(mp.find(key)==mp.end())
+            return "";
+        int l = 0, h = mp[key].size()-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(mp[key][mid].first < time)
+                l = mid + 1;
+            else if(mp[key][mid].first > time)
+                h = mid - 1;
+            else
+                return mp[key][mid].second;
+        }
+        if(h>=0)
+            return mp[key][h].second;
+        return "";
     }
 };
 
