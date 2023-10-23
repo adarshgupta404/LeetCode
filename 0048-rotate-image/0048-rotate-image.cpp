47221404adf1d3ec40271a0ce9a8552e179cc47a
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-10-23 10:23:06 +0530

    Time: 4 ms (41.26%), Space: 7.3 MB (62.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // Brute force
-        vector<vector<int>> secmat;
+        // Brute force O(n*n) Time and O(n*n) Space
+        // vector<vector<int>> secmat;
+        // int m = matrix.size();
+        // int n = matrix[0].size();
+        // for(int i = 0; i<m; i++){
+        //     vector<int> row;
+        //     for(int j = n-1; j>=0; j--){
+        //         row.push_back(matrix[j][i]);
+        //     } 
+        //     secmat.push_back(row);
+        // }
+        // matrix =  secmat;
+        // Brute force O(n*n) Time and O(1) Space
         int m = matrix.size();
         int n = matrix[0].size();
         for(int i = 0; i<m; i++){
-            vector<int> row;
-            for(int j = n-1; j>=0; j--){
-                row.push_back(matrix[j][i]);
+            for(int j = 0; j<=i; j++){
+                swap(matrix[i][j], matrix[j][i]);
             } 
-            secmat.push_back(row);
         }
-        matrix =  secmat;
+        for(int i=0;i<m;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
