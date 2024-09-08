3bc574e9408bbaae15c6eac771eb475f7d63f78d
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2024-09-08 15:51:14 +0530

    Time: 4 ms (8.58%), Space: 8.9 MB (5.28%) - LeetHub

---
+++ 1070-convert-to-base-2/1070-convert-to-base-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        string str = "";
+        while(n){
+            str = to_string(n & 1) +str;
+            n = -(n>>1);
+        }
+        return str == "" ? "0" : str;
+    }
+};
