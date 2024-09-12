aee0fc160e7fc377e23f3737f33519bdf68971ef
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2024-09-12 21:25:32 +0530

    Time: 3 ms (43.31%), Space: 9 MB (23.89%) - LeetHub

--- 1070-convert-to-base-2/1070-convert-to-base-2.cpp
+++ 1070-convert-to-base-2/1070-convert-to-base-2.cpp
@@ -4,6 +4,7 @@ public:
         string str = "";
         while(n){
             str = to_string(n & 1) +str;
+            cout<<n;
             n = -(n>>1);
         }
         return str == "" ? "0" : str;
