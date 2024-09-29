3557ec1d36e67644519a1263168dd3d5431f3e8b
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2024-09-30 00:01:25 +0530

    Time: 6 ms (88.38%), Space: 14.4 MB (37.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map <int, int> mp;
         int diff;
-        int i = 0;
-        for(auto num:nums){
-            diff = target - num;
+        for(int i=0; i<nums.size(); i++){
+            diff = target - nums[i];
             if(mp.find(diff)!=mp.end()){
                 return {mp[diff], i};
             }else{
-                mp.insert({num, i});
+                mp.insert({nums[i], i});
             }
-            i++;
         }
         return {};
     }
