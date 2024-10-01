616d37911e2a37171cc0645a26c3a43ed94a7980
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2024-10-01 14:52:11 +0530

    Time: 100 ms (37.91%), Space: 73.5 MB (50.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -10,6 +10,14 @@ public:
                 return true;
             }
         }
+        // bool flag = false;
+        // for(int i = 0; i<n; i++){
+        //     for(int j = i+1; j<n; j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
         return false;
     }
 };
