a7d9f3c0429fcfb2c7246b2c217d50b03079a701
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2024-10-13 11:43:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // Brute Force Solution
-        // int n = prices.size();
-        // int res = 0;
-        // for(int i = 0; i<n; i++){
-        //     for(int j = i+1; j<n && j!=n; j++){
-        //         if(prices[j]-prices[i]>=0)
-        //             res = max(res, prices[j]-prices[i]);
-        //     }
-        // }
-        // return res;
-
-        // Optimal Solution
-        // int mini = 100000;
-        // int profit = 0;
-        // int n = prices.size();
-        // for(int i = 0; i<n-1; i++){
-        //     mini = min(mini, prices[i]);
-        //     if(prices[i+1]-mini>0)
-        //         profit = max(profit, prices[i+1]-mini);
-        // }
-        // return profit;
-
-        //Two Pointer Technique
-        int l = 0, r = 1, profit = 0;
-        while(r<prices.size()){
-            if(prices[r]>prices[l]){
-                profit = max(profit, prices[r]-prices[l]);
-            }
-            else
-                l = r;
-            ++r;
+     int l = 0, r=1, prof = 0;
+     while(r<=prices.size()){
+        if(prices[r]-prices[l]>0){
+            prof = max(prof, prices[r]-prices[l]);
+        } else{
+            l = r;
         }
-        return profit;
+        r++;
+     }
+     return prof;
     }
 };
