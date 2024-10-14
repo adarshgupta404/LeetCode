eaee1aca6c0852392bede4b6e6d1a51fcff96939
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2024-10-14 22:04:23 +0530

    Time: 19 ms (69.41%), Space: 27.5 MB (92.21%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,12 +7,13 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;
-        if (k < 0)
-            k = n - k;
-        reverse(nums, 0, n - k - 1);
-        reverse(nums, n - k, n - 1);
-        reverse(nums, 0, n - 1);
+       int n = nums.size();
+       k = k % n;
+       if(k>n){
+        k = n-k;
+       }
+       reverse(nums, 0, n-k-1);
+       reverse(nums, n-k, n-1);
+       reverse(nums, 0, n-1);
     }
 };
