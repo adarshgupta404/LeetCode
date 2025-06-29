bdcbce5f8c7ebe5bfba9bd1b5cb1ba562bb34ff4
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2025-06-29 10:45:34 +0530

    Time: 73 ms (12.79%), Space: 57.4 MB (79.76%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,4 +1,22 @@
 class Solution {
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    int partition(int[] nums, int low, int high){
+        int pivot = nums[high];
+        int i = low;
+
+        for(int j = low; j < high; j++){
+            if(nums[j] > pivot){ 
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, high);
+        return i;
+    }
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
         for(int i : nums){
@@ -8,5 +26,15 @@ class Solution {
             pq.poll();
         }
         return pq.poll();
+        // int l = 0; 
+        // int h = nums.length-1;
+        // k = k-1;
+        // while(l<=h){
+        //     int pivot = partition(nums, l, h);
+        //     if(pivot == k) return nums[pivot];
+        //     else if(pivot<k) l = pivot+1;
+        //     else h = pivot-1;
+        // }
+        // return -1;
     }
 }
